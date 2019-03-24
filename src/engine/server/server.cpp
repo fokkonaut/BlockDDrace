a42b5e7900c76762dd6186a757c1036896b043fe
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2019-03-24 01:19:10 +0100

    fix server bots not having a clan

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -754,9 +754,9 @@ const char *CServer::ClientName(int ClientID)
 
 const char *CServer::ClientClan(int ClientID)
 {
-	if(ClientID < 0 || ClientID >= MAX_CLIENTS || m_aClients[ClientID].m_State == CServer::CClient::STATE_EMPTY || m_aClients[ClientID].m_State == CClient::STATE_BOT)
+	if(ClientID < 0 || ClientID >= MAX_CLIENTS || m_aClients[ClientID].m_State == CServer::CClient::STATE_EMPTY)
 		return "";
-	if(m_aClients[ClientID].m_State == CServer::CClient::STATE_INGAME)
+	if(m_aClients[ClientID].m_State == CServer::CClient::STATE_INGAME || m_aClients[ClientID].m_State == CServer::CClient::STATE_BOT)
 		return m_aClients[ClientID].m_aClan;
 	else
 		return "";
