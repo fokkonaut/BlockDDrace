0a656c127386f4e6684e9a068a4b18b3700d0ae1
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2019-03-24 12:46:38 +0100

    send 0 as score

--- src/game/server/player.cpp
+++ src/game/server/player.cpp
@@ -156,7 +156,7 @@ void CPlayer::Tick()
 	if (m_ChatScore > 0)
 		m_ChatScore--;
 
-	int Score = g_Config.m_SvBlockDDraceBrowserFix ? m_Level : m_Score;
+	int Score = g_Config.m_SvBlockDDraceBrowserFix ? m_Level : 0;
 	Server()->SetClientScore(m_ClientID, Score);
 
 	if (m_Moderating && m_Afk)
