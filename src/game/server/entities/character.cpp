faf92683f5c7cadca2371cf5f2e8cd3bb6705e4d
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2019-04-10 20:31:54 +0200

    might be better

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -1060,7 +1060,7 @@ void CCharacter::BlockDDraceTick()
 	}
 
 
-	CCharacter *pPas = GameServer()->m_World.ClosestCharacter(m_Pos, 40.0f, this);
+	CCharacter *pPas = GameServer()->m_World.ClosestCharacter(m_Pos, 50.0f, this);
 	if (pPas && pPas->m_Passive)
 	{
 		m_Core.m_Collision = false;
