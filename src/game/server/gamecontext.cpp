3052614cdcb7e9fb64bd4bd4e3f6b90507c0a778
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2019-04-23 14:13:01 +0200

    completely comment botmsg out

--- src/game/server/gamecontext.cpp
+++ src/game/server/gamecontext.cpp
@@ -1107,7 +1107,7 @@ void CGameContext::OnClientEnter(int ClientID)
 
 	Score()->CheckBirthday(ClientID);
 
-	{
+	/*{
 		int Empty = -1;
 		for(int i = 0; i < MAX_CLIENTS; i++)
 		{
@@ -1117,13 +1117,13 @@ void CGameContext::OnClientEnter(int ClientID)
 				break;
 			}
 		}
-		/*CNetMsg_Sv_Chat Msg;
+		CNetMsg_Sv_Chat Msg;
 		Msg.m_Team = 0;
 		Msg.m_ClientID = Empty;
 		Msg.m_pMessage = "Do you know someone who uses a bot? Please report them to the moderators.";
 		m_apPlayers[ClientID]->m_EligibleForFinishCheck = time_get();
-		Server()->SendPackMsg(&Msg, MSGFLAG_VITAL|MSGFLAG_NORECORD, ClientID);*/
-	}
+		Server()->SendPackMsg(&Msg, MSGFLAG_VITAL|MSGFLAG_NORECORD, ClientID);
+	}*/
 
 	if(!Server()->ClientPrevIngame(ClientID))
 	{
