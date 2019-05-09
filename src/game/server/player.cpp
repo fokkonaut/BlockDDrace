1a8da4892291b7c415ea0792541106dad26b40e6
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2019-05-09 20:42:06 +0200

    use character hasflag function

--- src/game/server/player.cpp
+++ src/game/server/player.cpp
@@ -257,7 +257,7 @@ void CPlayer::Tick()
 
 	if (m_Team != TEAM_SPECTATORS)
 	{
-		if (m_IsDummy && g_Config.m_SvHideBots == 2 && ((CGameControllerDDRace*)GameServer()->m_pController)->HasFlag(GetCharacter()) == -1)
+		if (m_IsDummy && g_Config.m_SvHideBots == 2 && GetCharacter()->HasFlag() == -1)
 			m_Team = TEAM_BLUE;
 		else if (m_IsDummy)
 			m_Team = TEAM_RED;
