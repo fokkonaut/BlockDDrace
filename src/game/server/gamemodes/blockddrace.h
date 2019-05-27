1d451fcf5a149b883c684eee47fbe583670596cf
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2019-05-27 13:57:03 +0200

    update header guard

--- src/game/server/gamemodes/blockddrace.h
+++ src/game/server/gamemodes/blockddrace.h
@@ -1,6 +1,6 @@
 /* (c) Shereef Marzouk. See "licence DDRace.txt" and the readme.txt in the root of the distribution for more information. */
-#ifndef GAME_SERVER_GAMEMODES_DDRACE_H
-#define GAME_SERVER_GAMEMODES_DDRACE_H
+#ifndef GAME_SERVER_GAMEMODES_BLOCKDDRACE_H
+#define GAME_SERVER_GAMEMODES_BLOCKDDRACE_H
 #include <game/server/gamecontroller.h>
 #include <game/server/teams.h>
 #include <game/server/entities/door.h>
@@ -36,4 +36,4 @@ public:
 	void InitTeleporter();
 	virtual void Tick();
 };
-#endif // GAME_SERVER_GAMEMODES_DDRACE_H
+#endif // GAME_SERVER_GAMEMODES_BLOCKDDRACE_H
