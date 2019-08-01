29c76153c6a0be864ce5c5bb4569009d074c9bbe
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2019-08-01 18:30:11 +0200

    Add show_ips

--- src/engine/server/server.h
+++ src/engine/server/server.h
@@ -174,6 +174,7 @@ public:
 		int m_AuthTries;
 		int m_NextMapChunk;
 		int m_Flags;
+		bool m_ShowIps;
 
 		const IConsole::CCommandInfo *m_pRconCmdToSend;
 
@@ -329,6 +330,7 @@ public:
 	static void ConStopRecord(IConsole::IResult *pResult, void *pUser);
 	static void ConMapReload(IConsole::IResult *pResult, void *pUser);
 	static void ConLogout(IConsole::IResult *pResult, void *pUser);
+	static void ConShowIps(IConsole::IResult *pResult, void *pUser);
 
 	static void ConAuthAdd(IConsole::IResult *pResult, void *pUser);
 	static void ConAuthAddHashed(IConsole::IResult *pResult, void *pUser);
