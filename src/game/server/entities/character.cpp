9734e8ef416257b28cbd1abda8c0ad96ba856d95
fokkonaut <35420825+fokkonaut@users.noreply.github.com>
2019-08-01 20:46:57 +0200

    Fix kill messages

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -1173,7 +1173,7 @@ void CCharacter::Die(int Killer, int Weapon)
 		Msg.m_ModeSpecial = ModeSpecial;
 		// BlockDDrace // only send kill message to players in the same minigame
 		for (int i = 0; i < MAX_CLIENTS; i++)
-			if (GameServer()->m_apPlayers[i] && m_pPlayer->m_Minigame == GameServer()->m_apPlayers[i]->m_Minigame)
+			if (!g_Config.m_SvHideMinigamePlayers || GameServer()->m_apPlayers[i] && m_pPlayer->m_Minigame == GameServer()->m_apPlayers[i]->m_Minigame)
 				Server()->SendPackMsg(&Msg, MSGFLAG_VITAL, i);
 	}
 
